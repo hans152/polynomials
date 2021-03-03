d271359fff6e9948b3f51c46564eff9e9f6ae56a
hans152 <79915792+hans152@users.noreply.github.com>
2021-03-03 17:13:55 +0100

    Add files via upload

---
+++ CirculantPolynomial.java
@@ -0,0 +1,215 @@
+package polynomial;
+
+/**
+ * 
+ * @author Andreas Rosowski
+ *
+ */
+public class CirculantPolynomial {
+	private static final long P = 2147483647L;
+	private static final long[] ONE = {1L, 0L};
+	private long[][] ROOTS;
+	private long[][] INVERSEROOTS;
+	
+	public long[] mult(long[] a, long[] b) {
+		int N = a.length + b.length - 1;
+		int d = 0;
+		while(N > 0) {
+			d++;
+			N >>= 1;
+		}
+		N = 1 << d;
+		long[] omega = {2L, 1L};
+		omega = CirculantPolynomial.power(omega, (CirculantPolynomial.P + 1) >> (d-3));
+		computeRoots(omega, N >> 3, d-3);
+		long[][] aTmp = new long[N][2];
+		long[] tmp = {a[0], 0};
+		aTmp[0] = tmp;
+		for(int i=1;i<a.length;i++) {
+			long[] tmp2 = {a[i], 0};
+			aTmp[N-i] = tmp2;
+		}
+		long[][] bTmp = new long[N][2];
+		for(int i=0;i<b.length;i++) {
+			long[] tmp2 = {b[i], 0};
+			bTmp[i] = tmp2;
+		}
+		long[][] cTmp = multRec(aTmp, bTmp, 0, N);
+		long[] c = new long[a.length + b.length - 1];
+		for(int i=0;i<c.length;i++) {
+			c[i] = (cTmp[i][0] + CirculantPolynomial.P) % CirculantPolynomial.P;
+		}
+		return c;
+	}
+	
+	private long[][] multRec(long[][] a, long[][] b, int D, int N) {
+		if(N <= 8) {
+			return multNaiv(a, b, D, N);
+		}
+		int newN = N >> 1;
+		long[][] a1 = new long[newN][2];
+		long[][] a2 = new long[newN][2];
+		long[][] b1 = new long[newN][2];
+		long[][] b2 = new long[newN][2];
+		long[][] M;
+		long[][] c;
+		long[] sqrt = ROOTS[D << 1];
+		
+		for(int i=0;i<newN;i++) {
+			a2[i] = CirculantPolynomial.multiplication(a[i+newN], sqrt);
+			a1[i][0] = (a[i][0] + a2[i][0]) % CirculantPolynomial.P;
+			a1[i][1] = (a[i][1] + a2[i][1]) % CirculantPolynomial.P;
+			b1[i] = CirculantPolynomial.multiplication(b[i], sqrt);
+			b2[i][0] = (b[i+newN][0] + b1[i][0]) % CirculantPolynomial.P;
+			b2[i][1] = (b[i+newN][1] + b1[i][1]) % CirculantPolynomial.P;
+		}
+		M = multRec(a1, b2, D << 1, newN);
+		
+		c = new long[N][2];
+		for(int i=0;i<newN;i++) {
+			c[i] = M[i];
+			a1[i][0] = (a[i][0] - a2[i][0]) % CirculantPolynomial.P;
+			a1[i][1] = (a[i][1] - a2[i][1]) % CirculantPolynomial.P;
+			b2[i][0] = (-b[i+newN][0] + b1[i][0]) % CirculantPolynomial.P;
+			b2[i][1] = (-b[i+newN][1] + b1[i][1]) % CirculantPolynomial.P;
+		}
+		M = multRec(a1, b2, (D << 1) + 1, newN);
+		
+		sqrt = INVERSEROOTS[D << 1];
+		for(int i=0;i<newN;i++) {
+			c[i][0] += M[i][0];
+			c[i][0] <<= 30;
+			c[i][0] %= CirculantPolynomial.P;
+			c[i][1] += M[i][1];
+			c[i][1] <<= 30;
+			c[i][1] %= CirculantPolynomial.P;
+			c[i+newN][0] = c[i][0] - M[i][0];
+			c[i+newN][0] %= CirculantPolynomial.P;
+			c[i+newN][1] = c[i][1] - M[i][1];
+			c[i+newN][1] %= CirculantPolynomial.P;
+			c[i] = CirculantPolynomial.multiplication(c[i], sqrt);
+		}
+		
+		return c;
+	}
+	
+	private long[][] multNaiv(long[][] a, long[][] b, int D, int N) {
+		long[][] c = new long[N][2];
+		int count = 0;
+		long[] sqrt = ROOTS[D];
+		for(int i=0;i<N;i++) {
+			long[] tmp = null;
+			for(int j=0;j<N-count;j++) {
+				tmp = CirculantPolynomial.multiplication(a[j], b[j+count]);
+				c[i][0] += tmp[0];
+				c[i][1] += tmp[1];
+			}
+			c[i][0] %= CirculantPolynomial.P;
+			c[i][1] %= CirculantPolynomial.P;
+			tmp[0] = 0L;
+			tmp[1] = 0L;
+			for(int j=N-count;j<N;j++) {
+				long[] tmp2 = CirculantPolynomial.multiplication(a[j], b[j-N+count]);
+				tmp[0] += tmp2[0];
+				tmp[1] += tmp2[1];
+			}
+			tmp[0] %= CirculantPolynomial.P;
+			tmp[1] %= CirculantPolynomial.P;
+			tmp = CirculantPolynomial.multiplication(tmp, sqrt);
+			c[i][0] = (c[i][0] + tmp[0]) % CirculantPolynomial.P;
+			c[i][1] = (c[i][1] + tmp[1]) % CirculantPolynomial.P;
+			count++;
+		}
+		return c;
+	}
+	
+	private void computeRoots(long[] omega, int N, int d) {
+		long[][] tmpRoots = new long[d][2];
+		long[] currOmega = omega;
+		for(int i=0;i<d;i++) {
+			tmpRoots[d-i-1] = currOmega;
+			currOmega = CirculantPolynomial.square(currOmega);
+		}
+		ROOTS = new long[N][2];
+		INVERSEROOTS = new long[N][2];
+		ROOTS[0] = currOmega;
+		INVERSEROOTS[0] = currOmega;
+		int countN = 1;
+		int countRoots = 0;
+		while(countN < N) {
+			currOmega = tmpRoots[countRoots];
+			for(int i=0;i<countN;i++) {
+				ROOTS[countN+i] = CirculantPolynomial.multiplication(ROOTS[i], currOmega);
+				INVERSEROOTS[(countN << 1)-1-i] = ROOTS[countN+i];
+			}
+			countRoots++;
+			countN <<= 1;
+		}
+	}
+	
+	private static long[] power(long[] a, long n) {
+		if(n == 0) {
+			return CirculantPolynomial.ONE;
+		}
+		else if(n == 1) {
+			return a;
+		}
+		int highestBit = 32;
+		int toAdd = 16;
+		while(toAdd > 0) {
+			long tmp = n >> highestBit;
+			if(tmp == 0) {
+				highestBit -= toAdd;
+			}
+			else if(tmp == 1) {
+				break;
+			}
+			else {
+				highestBit += toAdd;
+			}
+			toAdd >>= 1;
+		}
+		highestBit--;
+		int checkBit = 1 << highestBit;
+		long[] result = new long[2];
+		result[0] = a[0];
+		result[1] = a[1];
+		while(checkBit > 0) {
+			result = CirculantPolynomial.square(result);
+			if((n & checkBit) == checkBit) {
+				result = CirculantPolynomial.multiplication(result, a);
+			}
+			checkBit >>= 1;
+		}
+		return result;
+	}
+	
+	private static long[] multiplication(long[] a, long[] b) {
+		long tmp1 = (a[0] * b[0]) % CirculantPolynomial.P;
+		long tmp2 = (a[1] * b[1]) % CirculantPolynomial.P;
+		tmp2 *= 3;
+		tmp2 %= CirculantPolynomial.P;
+		tmp1 += tmp2;
+		tmp1 %= CirculantPolynomial.P;
+		long tmp3 = (a[0] * b[1]) % CirculantPolynomial.P;
+		long tmp4 = (a[1] * b[0]) % CirculantPolynomial.P;
+		tmp3 += tmp4;
+		tmp3 %= CirculantPolynomial.P;
+		long[] result = {tmp1, tmp3};
+		return result;
+	}
+	
+	private static long[] square(long[] a) {
+		long tmp1 = (a[0] * a[0]) % CirculantPolynomial.P;
+		long tmp2 = (a[1] * a[1]) % CirculantPolynomial.P;
+		tmp2 *= 3;
+		tmp2 %= CirculantPolynomial.P;
+		tmp1 += tmp2;
+		tmp1 %= CirculantPolynomial.P;
+		long tmp3 = (a[0] * a[1]) % CirculantPolynomial.P;
+		tmp3 <<= 1;
+		tmp3 %= CirculantPolynomial.P;
+		long[] result = {tmp1, tmp3};
+		return result;
+	}
+}
---
+++ ClassicalPolynomial.java
@@ -0,0 +1,165 @@
+package polynomial;
+
+/**
+ * 
+ * @author Andreas Rosowski
+ *
+ */
+public class ClassicalPolynomial {
+	private static final long P = 2147483647L;
+	private static final long[] ONE = {1L, 0L};
+	
+	public long[] mult(long[] a, long[] b) {
+		int N = a.length + b.length - 1;
+		int d = 0;
+		while(N > 0) {
+			d++;
+			N >>= 1;
+		}
+		N = 1 << d;
+		long[] omega = {2L, 1L};
+		omega = ClassicalPolynomial.power(omega, (ClassicalPolynomial.P + 1) >> d);
+		long[][] aTmp = new long[N][2];
+		for(int i=0;i<a.length;i++) {
+			long[] tmp = {a[i], 0};
+			aTmp[i] = tmp;
+		}
+		long[][] bTmp = new long[N][2];
+		for(int i=0;i<b.length;i++) {
+			long[] tmp = {b[i], 0};
+			bTmp[i] = tmp;
+		}
+		aTmp = FFT(aTmp, omega, N);
+		bTmp = FFT(bTmp, omega, N);
+		long[][] cTmp = new long[N][2];
+		for(int i=0;i<N;i++) {
+			cTmp[i] = ClassicalPolynomial.multiplication(aTmp[i], bTmp[i]);
+		}
+		omega = ClassicalPolynomial.power(omega, N-1);
+		cTmp = FFT(cTmp, omega, N);
+		long[] c = new long[a.length + b.length - 1];
+		long[] inverseN = {(ClassicalPolynomial.P + 1) >> d, 0L};
+		for(int i=0;i<c.length;i++) {
+			long[] tmp = ClassicalPolynomial.multiplication(cTmp[i], inverseN);
+			c[i] = (tmp[0] + ClassicalPolynomial.P) % ClassicalPolynomial.P;
+		}
+		return c;
+	}
+	
+	private long[][] FFT(long[][] a, long[] omega, int N) {
+		if(N <= 4) {
+			return DFT(a, omega, N);
+		}
+		int newN = N >> 1;
+		long[] newOmega = ClassicalPolynomial.square(omega);
+		long[][] a0 = new long[newN][2];
+		for(int i=0;i<newN;i++) {
+			a0[i] = a[i << 1];
+		}
+		a0 = FFT(a0, newOmega, newN);
+		long[][] a1 = new long[newN][2];
+		for(int i=0;i<newN;i++) {
+			a1[i] = a[(i << 1) + 1];
+		}
+		a1 = FFT(a1, newOmega, newN);
+		long[][] c = new long[N][2];
+		c[0][0] = (a0[0][0] + a1[0][0]) % ClassicalPolynomial.P;
+		c[0][1] = (a0[0][1] + a1[0][1]) % ClassicalPolynomial.P;
+		c[newN][0] = (a0[0][0] - a1[0][0]) % ClassicalPolynomial.P;
+		c[newN][1] = (a0[0][1] - a1[0][1]) % ClassicalPolynomial.P;
+		long[] currOmega = omega;
+		for(int i=1;i<newN;i++) {
+			long[] tmp = ClassicalPolynomial.multiplication(a1[i], currOmega);
+			c[i][0] = (tmp[0] + a0[i][0]) % ClassicalPolynomial.P;
+			c[i][1] = (tmp[1] + a0[i][1]) % ClassicalPolynomial.P;
+			c[i+newN][0] = (a0[i][0] - tmp[0]) % ClassicalPolynomial.P;
+			c[i+newN][1] = (a0[i][1] - tmp[1]) % ClassicalPolynomial.P;
+			currOmega = ClassicalPolynomial.multiplication(currOmega, omega);
+		}
+		return c;
+	}
+	
+	private long[][] DFT(long[][] a, long[] omega, int N) {
+		long[] currOmega = ClassicalPolynomial.ONE;
+		long[] toMultiplyOmega = ClassicalPolynomial.ONE;
+		long[][] c = new long[N][2];
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				long[] tmp = ClassicalPolynomial.multiplication(a[j], currOmega);
+				c[i][0] += tmp[0];
+				c[i][1] += tmp[1];
+				currOmega = ClassicalPolynomial.multiplication(currOmega, toMultiplyOmega);
+			}
+			c[i][0] %= ClassicalPolynomial.P;
+			c[i][1] %= ClassicalPolynomial.P;
+			toMultiplyOmega = ClassicalPolynomial.multiplication(toMultiplyOmega, omega);
+			currOmega = ClassicalPolynomial.ONE;
+		}
+		return c;
+	}
+	
+	private static long[] power(long[] a, long n) {
+		long[] result = new long[2];
+		if(n == 0) {
+			return ClassicalPolynomial.ONE;
+		}
+		else if(n == 1) {
+			return a;
+		}
+		int highestBit = 32;
+		int toAdd = 16;
+		while(toAdd > 0) {
+			long tmp = n >> highestBit;
+			if(tmp == 0) {
+				highestBit -= toAdd;
+			}
+			else if(tmp == 1) {
+				break;
+			}
+			else {
+				highestBit += toAdd;
+			}
+			toAdd >>= 1;
+		}
+		highestBit--;
+		int checkBit = 1 << highestBit;
+		result = a;
+		while(checkBit > 0) {
+			result = ClassicalPolynomial.square(result);
+			if((n & checkBit) == checkBit) {
+				result = ClassicalPolynomial.multiplication(result, a);
+			}
+			checkBit >>= 1;
+		}
+		return result;
+	}
+	
+	private static long[] multiplication(long[] a, long[] b) {
+		long tmp1 = (a[0] * b[0]) % ClassicalPolynomial.P;
+		long tmp2 = (a[1] * b[1]) % ClassicalPolynomial.P;
+		tmp2 *= 3;
+		tmp2 %= ClassicalPolynomial.P;
+		tmp1 += tmp2;
+		tmp1 %= ClassicalPolynomial.P;
+		long tmp3 = (a[0] * b[1]) % ClassicalPolynomial.P;
+		long tmp4 = (a[1] * b[0]) % ClassicalPolynomial.P;
+		tmp3 += tmp4;
+		tmp3 %= ClassicalPolynomial.P;
+		long[] result = {tmp1, tmp3};
+		return result;
+	}
+	
+	private static long[] square(long[] a) {
+		long tmp1 = (a[0] * a[0]) % ClassicalPolynomial.P;
+		long tmp2 = (a[1] * a[1]) % ClassicalPolynomial.P;
+		tmp2 *= 3;
+		tmp2 %= ClassicalPolynomial.P;
+		tmp1 += tmp2;
+		tmp1 %= ClassicalPolynomial.P;
+		long tmp3 = (a[0] * a[1]) % ClassicalPolynomial.P;
+		tmp3 <<= 1;
+		tmp3 %= ClassicalPolynomial.P;
+		long[] result = {tmp1, tmp3};
+		return result;
+	}
+}
