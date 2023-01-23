f6e081557bf84274687876d2e64ade7898f09ca4
hans152 <79915792+hans152@users.noreply.github.com>
2023-01-23 10:21:08 +0100

    Update ClassicalPolynomial.java

--- ClassicalPolynomial.java
+++ ClassicalPolynomial.java
@@ -6,6 +6,9 @@
 public class ClassicalPolynomial {
 	private static final long P = 2147483647L;
 	private static final long[] ONE = {1L, 0L};
+	private long[][] ROOTS;
+	private long[][][] TMP1;
+	private long[][][] TMP2;
 	
 	public long[] mult(long[] a, long[] b) {
 		int N = a.length + b.length - 1;
@@ -15,8 +18,11 @@ public class ClassicalPolynomial {
 			N >>= 1;
 		}
 		N = 1 << d;
+		TMP1 = new long[d][][];
+		TMP2 = new long[d][][];
 		long[] omega = {2L, 1L};
 		omega = ClassicalPolynomial.power(omega, (ClassicalPolynomial.P + 1) >> d);
+		computeRoots(omega, N);
 		long[][] aTmp = new long[N][2];
 		for(int i=0;i<a.length;i++) {
 			long[] tmp = {a[i], 0};
@@ -27,77 +33,132 @@ public class ClassicalPolynomial {
 			long[] tmp = {b[i], 0};
 			bTmp[i] = tmp;
 		}
-		aTmp = FFT(aTmp, omega, N);
-		bTmp = FFT(bTmp, omega, N);
+		aTmp = FFT(aTmp, 1, N, 1, 0, 0, true);
+		bTmp = FFT(bTmp, 1, N, 1, 0, 0, false);
 		long[][] cTmp = new long[N][2];
 		for(int i=0;i<N;i++) {
 			cTmp[i] = ClassicalPolynomial.multiplication(aTmp[i], bTmp[i]);
 		}
 		omega = ClassicalPolynomial.power(omega, N-1);
-		cTmp = FFT(cTmp, omega, N);
+		cTmp = FFTinverse(cTmp, -1, N, 1, 0, 0, true);
 		long[] c = new long[a.length + b.length - 1];
-		long[] inverseN = {(ClassicalPolynomial.P + 1) >> d, 0L};
 		for(int i=0;i<c.length;i++) {
-			long[] tmp = ClassicalPolynomial.multiplication(cTmp[i], inverseN);
-			c[i] = (tmp[0] + ClassicalPolynomial.P) % ClassicalPolynomial.P;
+			cTmp[i][0] *= (ClassicalPolynomial.P + 1) >> d;
+			cTmp[i][0] %= ClassicalPolynomial.P;
+			c[i] = (cTmp[i][0] + ClassicalPolynomial.P) % ClassicalPolynomial.P;
 		}
 		return c;
 	}
 	
-	private long[][] FFT(long[][] a, long[] omega, int N) {
-		if(N <= 4) {
-			return DFT(a, omega, N);
+	private long[][] FFT(long[][] a, int D, int N, int length, int offset, int depth, boolean isFirst) {
+		if(N <= 2) {
+			return DFTN2(a, length, offset, depth, isFirst);
 		}
 		int newN = N >> 1;
-		long[] newOmega = ClassicalPolynomial.square(omega);
-		long[][] a0 = new long[newN][2];
-		for(int i=0;i<newN;i++) {
-			a0[i] = a[i << 1];
+		int newD = D << 1;
+		long[][] a0 = FFT(a, newD, newN, length << 1, offset, depth+1, true);
+		long[][] a1 = FFT(a, newD, newN, length << 1, offset+length, depth+1, false);
+		long[][] c;
+		if(isFirst) {
+			if(TMP1[depth] == null) {
+				TMP1[depth] = new long[N][2];
+			}
+			c = TMP1[depth];
 		}
-		a0 = FFT(a0, newOmega, newN);
-		long[][] a1 = new long[newN][2];
-		for(int i=0;i<newN;i++) {
-			a1[i] = a[(i << 1) + 1];
+		else {
+			if(TMP2[depth] == null) {
+				TMP2[depth] = new long[N][2];
+			}
+			c = TMP2[depth];
 		}
-		a1 = FFT(a1, newOmega, newN);
-		long[][] c = new long[N][2];
 		c[0][0] = (a0[0][0] + a1[0][0]) % ClassicalPolynomial.P;
 		c[0][1] = (a0[0][1] + a1[0][1]) % ClassicalPolynomial.P;
 		c[newN][0] = (a0[0][0] - a1[0][0]) % ClassicalPolynomial.P;
 		c[newN][1] = (a0[0][1] - a1[0][1]) % ClassicalPolynomial.P;
-		long[] currOmega = omega;
+		int currD = D;
+		long[] currOmega = ROOTS[currD];
 		for(int i=1;i<newN;i++) {
 			long[] tmp = ClassicalPolynomial.multiplication(a1[i], currOmega);
 			c[i][0] = (tmp[0] + a0[i][0]) % ClassicalPolynomial.P;
 			c[i][1] = (tmp[1] + a0[i][1]) % ClassicalPolynomial.P;
 			c[i+newN][0] = (a0[i][0] - tmp[0]) % ClassicalPolynomial.P;
 			c[i+newN][1] = (a0[i][1] - tmp[1]) % ClassicalPolynomial.P;
-			currOmega = ClassicalPolynomial.multiplication(currOmega, omega);
+			currD += D;
+			currOmega = ROOTS[currD];
 		}
 		return c;
 	}
 	
-	private long[][] DFT(long[][] a, long[] omega, int N) {
-		long[] currOmega = ClassicalPolynomial.ONE;
-		long[] toMultiplyOmega = ClassicalPolynomial.ONE;
-		long[][] c = new long[N][2];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				long[] tmp = ClassicalPolynomial.multiplication(a[j], currOmega);
-				c[i][0] += tmp[0];
-				c[i][1] += tmp[1];
-				currOmega = ClassicalPolynomial.multiplication(currOmega, toMultiplyOmega);
+	private long[][] FFTinverse(long[][] a, int D, int N, int length, int offset, int depth, boolean isFirst) {
+		if(N <= 2) {
+			return DFTN2(a, length, offset, depth, isFirst);
+		}
+		int newN = N >> 1;
+		int newD = D << 1;
+		long[][] a0 = FFTinverse(a, newD, newN, length << 1, offset, depth+1, true);
+		long[][] a1 = FFTinverse(a, newD, newN, length << 1, offset+length, depth+1, false);
+		long[][] c;
+		if(isFirst) {
+			if(TMP1[depth] == null) {
+				TMP1[depth] = new long[N][2];
 			}
-			c[i][0] %= ClassicalPolynomial.P;
-			c[i][1] %= ClassicalPolynomial.P;
-			toMultiplyOmega = ClassicalPolynomial.multiplication(toMultiplyOmega, omega);
-			currOmega = ClassicalPolynomial.ONE;
+			c = TMP1[depth];
+		}
+		else {
+			if(TMP2[depth] == null) {
+				TMP2[depth] = new long[N][2];
+			}
+			c = TMP2[depth];
+		}
+		c[0][0] = (a0[0][0] + a1[0][0]) % ClassicalPolynomial.P;
+		c[0][1] = (a0[0][1] + a1[0][1]) % ClassicalPolynomial.P;
+		c[newN][0] = (a0[0][0] - a1[0][0]) % ClassicalPolynomial.P;
+		c[newN][1] = (a0[0][1] - a1[0][1]) % ClassicalPolynomial.P;
+		int currD = D;
+		long[] currOmega = ROOTS[ROOTS.length+currD];
+		for(int i=1;i<newN;i++) {
+			long[] tmp = ClassicalPolynomial.multiplication(a1[i], currOmega);
+			c[i][0] = (tmp[0] + a0[i][0]) % ClassicalPolynomial.P;
+			c[i][1] = (tmp[1] + a0[i][1]) % ClassicalPolynomial.P;
+			c[i+newN][0] = (a0[i][0] - tmp[0]) % ClassicalPolynomial.P;
+			c[i+newN][1] = (a0[i][1] - tmp[1]) % ClassicalPolynomial.P;
+			currD += D;
+			currOmega = ROOTS[ROOTS.length+currD];
 		}
 		return c;
+	}
+	
+	private long[][] DFTN2(long[][] a, int length, int offset, int depth, boolean isFirst) {
+		long[][] c;
+		if(isFirst) {
+			if(TMP1[depth] == null) {
+				TMP1[depth] = new long[2][2];
+			}
+			c = TMP1[depth];
+		}
+		else {
+			if(TMP2[depth] == null) {
+				TMP2[depth] = new long[2][2];
+			}
+			c = TMP2[depth];
+		}
+		c[0][0] = (a[offset][0] + a[offset+length][0]) % ClassicalPolynomial.P;
+		c[0][1] = (a[offset][1] + a[offset+length][1]) % ClassicalPolynomial.P;
+		c[1][0] = (a[offset][0] - a[offset+length][0]) % ClassicalPolynomial.P;
+		c[1][1] = (a[offset][1] - a[offset+length][1]) % ClassicalPolynomial.P;
+		return c;
+	}
+	
+	private void computeRoots(long[] omega, int N) {
+		ROOTS = new long[N][2];
+		ROOTS[0][0] = 1L;
+		ROOTS[0][1] = 0L;
+		for(int i=1;i<N;i++) {
+			ROOTS[i] = ClassicalPolynomial.multiplication(omega, ROOTS[i-1]);
+		}
 	}
 	
 	private static long[] power(long[] a, long n) {
-		long[] result = new long[2];
 		if(n == 0) {
 			return ClassicalPolynomial.ONE;
 		}
@@ -121,7 +182,7 @@ public class ClassicalPolynomial {
 		}
 		highestBit--;
 		int checkBit = 1 << highestBit;
-		result = a;
+		long[] result = a;
 		while(checkBit > 0) {
 			result = ClassicalPolynomial.square(result);
 			if((n & checkBit) == checkBit) {
